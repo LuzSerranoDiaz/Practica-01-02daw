ccdb0d1eb95ea54488e337ad446bee2069a59874
LuzSerranoDiaz <diaz03luz@gmail.com>
2023-11-01 19:20:03 +0000

    Cambios menores

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -36,15 +36,3 @@ mysql -u root < /tmp/iaw-practica-lamp/db/database.sql
 mysql -u root <<< "DROP USER IF EXISTS $DB_USER@'%'";
 mysql -u root <<< "CREATE USER $DB_USER@'%' IDENTIFIED BY '$DB_PASS'";
 mysql -u root <<< "GRANT ALL PRIVILEGES ON $DB_NAME.* TO $DB_USER@'%'";
-
-#Creamos la certificacion autofirmado
-
-# Creamos el certificado autofirmado
-sudo openssl req \
-  -x509 \
-  -nodes \
-  -days 365 \
-  -newkey rsa:2048 \
-  -keyout /etc/ssl/private/apache-selfsigned.key \
-  -out /etc/ssl/certs/apache-selfsigned.crt \
-  -subj "/C=$OPENSSL_COUNTRY/ST=$OPENSSL_PROVINCE/L=$OPENSSL_LOCALITY/O=$OPENSSL_ORGANIZATION/OU=$OPENSSL_ORGUNIT/CN=$OPENSSL_COMMON_NAME/emailAddress=$OPENSSL_EMAIL"
